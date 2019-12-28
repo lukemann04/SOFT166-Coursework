a81b295b5f781b5bb9df2d7bdaf44ee169bd7f25
lukemann04 <lukemann04@gmail.com>
2019-12-28 16:11:03 +0000

    User Input Check
    Added a function to compare the user's input to the sequence.

--- js/gameScript.js
+++ js/gameScript.js
@@ -1,6 +1,14 @@
 var colours = [];
+var userInput = [];
 var gameTimer;
 var sequenceNumber = 0;
+var currentLight = 0;
+var result = "";
+
+function selectedLight(l) {
+    currentLight = l;
+}
+
 function selectNextColour()
 {
     sequenceNumber=0;
@@ -8,18 +16,23 @@ function selectNextColour()
     if (nextColour == 1)
     {
         colours.push("red");
+        setLights(currentLight, 0, 'On', 100, 254)
+
     }
     else if (nextColour == 2)
     {
         colours.push("green");
+        setLights(currentLight, 25500, 'On', 100, 254)
     }
     else if (nextColour == 3)
     {
         colours.push("blue");
+        setLights(currentLight, 46920, 'On', 100, 254)
     }
     else if (nextColour == 4)
     {
         colours.push("yellow");
+        setLights(currentLight, 12750, 'On', 100, 254)
     }
 
     if (colours[colours.length-1]==colours[colours.length-2]){
@@ -49,3 +62,35 @@ function showColour()
         sequenceNumber++;
     }
 
+
+function saveInput(num)
+    {
+       if (num == 1)
+       {
+           userInput.push("red");
+       }
+       if (num == 2)
+       {
+           userInput.push("Green");
+       }
+        if (num == 3)
+        {
+            userInput.push("blue");
+        }
+        if (num == 4)
+        {
+            userInput.push("yellow");
+        }
+        checkInput()
+    }
+
+function checkInput()
+    {
+        if (colours.length == userInput.length) {
+            if (JSON.stringify(colours) == JSON.stringify(userInput)) {
+                result = "correct"
+            } else {
+                result = "incorrect"
+            }
+        }
+    }
